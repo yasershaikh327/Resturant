4f77a6777a261094e11e4f9566ffb89385bcdc53
Yaser Shaikh <syaser327@gmail.com>
2026-02-17 23:34:55 +0530

    Updated

--- Resturant/Resturant.Presentation.Server/Program.cs
+++ Resturant/Resturant.Presentation.Server/Program.cs
@@ -3,6 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:8080",                         // React dev server
+                "https://ai-powered-resturant-ordering-system.onrender.com" // Render frontend
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
